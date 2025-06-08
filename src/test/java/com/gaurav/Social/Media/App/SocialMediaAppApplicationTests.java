3637104ab85189ac4e449f8c9c7619205f736ba7
gauravv420p <gp12e45@gmail.com>
2025-06-08 13:55:10 +0530

    initial setup done

---
+++ src/test/java/com/gaurav/Social/Media/App/SocialMediaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gaurav.Social.Media.App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialMediaAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
